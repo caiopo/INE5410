be6720b21eb0d7d14a1fc8797ebfb0501a5f645d
caiopo <caiopoliveira@gmail.com>
2016-05-10 09:16:44 -0300

    T1 working, but not done

--- Trabalho1/kmp.c
+++ Trabalho1/kmp.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <pthread.h>
 
-
 #define RANDNUM_W 521288629;
 #define RANDNUM_Z 362436069;
 
@@ -19,7 +18,7 @@ void srandnum(int seed) {
 	randum_z = (z) ? z : RANDNUM_Z;
 }
 
-unsigned int randnum(void) {
+unsigned int randnum() {
 	unsigned int u;
 	randum_z = 36969 * (randum_z & 65535) + (randum_z >> 16);
 	randum_w = 18000 * (randum_w & 65535) + (randum_w >> 16);
@@ -27,11 +26,148 @@ unsigned int randnum(void) {
 	return (u);
 }
 
+typedef float *vector_t;
 
-int main(int argc, char const *argv[]) {
-	if (argc != 6) {
-		printf("Usage: npoints dimension ncentroids mindistance seed\n");
-		exit (1);
+int npoints;
+int dimension;
+int ncentroids;
+int nthreads;
+float mindistance;
+int seed;
+vector_t *data, *centroids;
+int *map;
+int *dirty;
+int too_far;
+int has_changed;
+
+float v_distance(vector_t a, vector_t b) {
+	int i;
+	float distance = 0;
+	for (i = 0; i < dimension; i++)
+		distance += pow(a[i] - b[i], 2);
+	return sqrt(distance);
+}
+
+static void populate2(void* initv) {
+		int init = (int) initv;
+		int j;
+		float tmp;
+		float distance;
+		int min = (init * npoints)/nthreads;
+		int max = ((init+1) * npoints)/nthreads;
+
+		for (int i = min; i < max; ++i) {
+			distance = v_distance(centroids[map[i]], data[i]);
+			/* Look for closest cluster. */
+			for (j = 0; j < ncentroids; j++) {
+				/* Point is in this cluster. */
+				if (j == map[i])
+					continue;
+				tmp = v_distance(centroids[j], data[i]);
+				if (tmp < distance) {
+					map[i] = j;
+					distance = tmp;
+					dirty[j] = 1;
+				}
+			}
+		}
+		/* Cluster is too far away. */
+		if (distance > mindistance)
+			too_far = 1;
+
+		pthread_exit(NULL);
+}
+
+static void populate() {
+	too_far = 0;
+
+	int i;
+
+	pthread_t* threads = malloc(sizeof(pthread_t) * nthreads);
+
+	for (i = 0; i < nthreads; i++) {
+		pthread_create(&threads[i], NULL, populate2, i);
+	}
+
+	for (i = 0; i < nthreads; i++) {
+		pthread_join(threads[i], NULL);
+	}
+}
+
+static void compute_centroids() {
+	int i, j, k;
+	int population;
+	has_changed = 0;
+	/* Compute means. */
+	for (i = 0; i < ncentroids; i++) {
+		if (!dirty[i])
+			continue;
+		memset (centroids[i], 0, sizeof (float) * dimension);
+		/* Compute cluster's mean. */
+		population = 0;
+		for (j = 0; j < npoints; j++) {
+			if (map[j] != i)
+				continue;
+			for (k = 0; k < dimension; k++)
+				centroids[i][k] += data[j][k];
+			population++;
+		}
+		if (population > 1) {
+			for (k = 0; k < dimension; k++)
+				centroids[i][k] *= 1.0 / population;
+		}
+		has_changed = 1;
+	}
+	memset (dirty, 0, ncentroids * sizeof (int));
+}
+
+int* kmeans() {
+	int i, j, k;
+	too_far = 0;
+	has_changed = 0;
+
+	if (!(map = calloc (npoints, sizeof (int))))
+		exit(1);
+	if (!(dirty = malloc (ncentroids * sizeof (int))))
+		exit(1);
+	if (!(centroids = malloc (ncentroids * sizeof (vector_t))))
+		exit(1);
+
+	for (i = 0; i < ncentroids; i++)
+		centroids[i] = malloc (sizeof (float) * dimension);
+	for (i = 0; i < npoints; i++)
+		map[i] = -1;
+	for (i = 0; i < ncentroids; i++) {
+		dirty[i] = 1;
+		j = randnum() % npoints;
+		for (k = 0; k < dimension; k++)
+			centroids[i][k] = data[j][k];
+		map[j] = i;
+	}
+	/* Map unmapped data points. */
+	for (i = 0; i < npoints; i++)
+		if (map[i] < 0)
+			map[i] = randnum () % ncentroids;
+
+	do {              /* Cluster data. */
+		populate();
+		compute_centroids();
+	} while (too_far && has_changed);
+
+	for (i = 0; i < ncentroids; i++)
+		free(centroids[i]);
+	free(centroids);
+	free(dirty);
+
+	return map;
+}
+
+int main (int argc, char** argv) {
+	int i, j;
+
+	if (argc != 7) {
+		printf ("Usage: npoints dimension ncentroids mindistance seed maxthreads\n");
+		exit(1);
 	}
 
 	npoints = atoi(argv[1]);
@@ -39,9 +175,33 @@ int main(int argc, char const *argv[]) {
 	ncentroids = atoi(argv[3]);
 	mindistance = atoi(argv[4]);
 	seed = atoi(argv[5]);
+	nthreads = atoi(argv[6]);
 
 	srandnum(seed);
 
+	if (!(data = malloc (npoints * sizeof(vector_t))))
+		exit(1);
 
-	return 0;
+	for (i = 0; i < npoints; i++) {
+		data[i] = malloc(sizeof(float) * dimension);
+		for (j = 0; j < dimension; j++)
+			data[i][j] = randnum() & 0xffff;
+	}
+
+	map = kmeans();
+
+	for (i = 0; i < ncentroids; i++) {
+		printf ("\nPartition %d:\n", i);
+		for (j = 0; j < npoints; j++)
+			if (map[j] == i)
+				printf("%d ", j);
+	}
+	printf("\n");
+
+	free(map);
+	for (i = 0; i < npoints; i++)
+		free(data[i]);
+	free(data);
+
+	return (0);
 }
